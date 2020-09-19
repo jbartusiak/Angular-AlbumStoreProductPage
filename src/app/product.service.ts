8743453c3ff4636186bc1cd0049812f1d37075d1
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-09-19 17:59:55 +0200

    Pt3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { map } from 'rxjs/operators';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,9 +11,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(
-      map(response => response.json()),
-    )
+    return this._http.get(this._albumUrl).map((response: Response) => response.json());
   }
-
 }
